108d7487b18b04285f9ba7f2232e1a1722a639a5
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-07 09:48:51 +0200

    Implemented load_ROM_to_memory. Loading needs to be improved: - Check if we're loading to correct memory address - Check ROM headers for metadata on how to handle addressing for the ROM

---
+++ include/utils.h
@@ -0,0 +1,6 @@
+#ifndef _UTILS_H
+#define _UTILS_H
+
+int load_ROM_to_memory(uint8_t memory[], size_t memory_size, char* rom_path);
+
+#endif // _UTILS_H
--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include "CPU.h"
 #include "memory.h"
+#include "utils.h"
 
 int main(int argc, char* argv[]) {
 	printf("GameBoy emulator started.\n");
 	if (argc != 2) {
-		printf("Usage: ./%s <rom_path>\n", argv[0]);
+		printf("Usage: %s <rom_path>\n", argv[0]);
 		exit(1);
 	}
 	char* rom_path = argv[1]; // TODO: Implement proper arg parsing
@@ -16,7 +18,7 @@ int main(int argc, char* argv[]) {
 
 	uint8_t memory[MEMORY_SIZE] = {0};
 
-	if (!load_ROM_to_memory(memory, sizeof(memory), rom_path)) {
+	if (load_ROM_to_memory(memory, sizeof(memory), rom_path) < 0) {
 		printf("ERROR: Could not load rom <%s> into memory. Exiting...\n", rom_path);
 	}
 	bool running = true;
