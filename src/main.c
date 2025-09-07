eefaf92b7171ccb8ae3bc5792b9bf8752dab131b
Aleksa Vidovic <aleksavidovic8@gmail.com>
2025-09-07 05:37:38 +0200

    Added basic args check: program now expects a ROM path. Added a placeholder while loop == this will be the main program loop. Added placeholder ROM loading code. utils.c/load_ROM_to_memory still needs to be properly implemented.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "CPU.h"
 #include "memory.h"
 
-int main(void) {
+int main(int argc, char* argv[]) {
 	printf("GameBoy emulator started.\n");
+	if (argc != 2) {
+		printf("Usage: ./%s <rom_path>\n", argv[0]);
+		exit(1);
+	}
+	char* rom_path = argv[1]; // TODO: Implement proper arg parsing
+
 	CPU cpu;
 	CPU_init(&cpu);
+
 	uint8_t memory[MEMORY_SIZE] = {0};
+
+	if (!load_ROM_to_memory(memory, sizeof(memory), rom_path)) {
+		printf("ERROR: Could not load rom <%s> into memory. Exiting...\n", rom_path);
+	}
+	bool running = true;
+	while (running) {
+		// 1. Fetch
+		// 2. Decode
+		// 3. Execute
+		running = false;
+	}
 	return 0;
 }
--- src/utils.c
+++ src/utils.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int load_ROM_to_memory(uint8_t memory[], size_t memory_size, char* rom_filename) {
-	printf("Loading ROM: %s\n", rom_filename);
+int load_ROM_to_memory(uint8_t memory[], size_t memory_size, char* rom_path) {
+	printf("Loading ROM: %s\n", rom_path);
 	return 0;
 }
